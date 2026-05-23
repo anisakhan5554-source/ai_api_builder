67974d772cc5bc075d028e4e637d8ee92ac3f1dd
Anisa Nabi <anisakhan5554@gmail.com>
2026-05-23 04:55:01 -0700

    Added CORS middleware

--- main.py
+++ main.py
@@ -9,8 +9,30 @@ from route.auth import router as auth_router
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from  fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware( CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler)
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(users_router, prefix="/api/v1")
+app.include_router(auth_router, prefix="/api/v1")
+
+
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
@@ -32,13 +54,6 @@ async def global_exception_handler(request: Request, exc: Exception):
         }
     )
 
-app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler)
-
-Base.metadata.create_all(bind=engine)
-
-app.include_router(users_router, prefix="/api/v1")
-app.include_router(auth_router, prefix="/api/v1")
 
 @app.get("/")
 def root():
