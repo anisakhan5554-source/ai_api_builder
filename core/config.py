9d6cfd809040461fe9be71629882320afc19bde6
Anisa Nabi <anisakhan5554@gmail.com>
2026-05-25 03:55:10 -0700

    fixed railway env setup

--- core/config.py
+++ core/config.py
@@ -6,3 +6,8 @@ import os
 load_dotenv()
 
 SECRET_KEY=os.getenv("SECRET_KEY","fallback_secret")
+DATABASE_URL = os.getenv("DATABASE_URL")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+)
