e096885483b96c3318d0077d81005554f62665af
Anisa Nabi <anisakhan5554@gmail.com>
2026-06-21 09:14:13 -0700

    Debug redis env vars on railway

--- core/redis_client.py
+++ core/redis_client.py
@@ -4,4 +4,7 @@ from upstash_redis import Redis
 REDIS_URL = os.environ.get("REDIS_URL")
 REDIS_TOKEN = os.environ.get("REDIS_TOKEN")
 
+print("RAILWAY REDIS_URL:", repr(REDIS_URL))
+print("RAILWAY REDIS_TOKEN:", repr(REDIS_TOKEN))
+
 redis_client = Redis(url=REDIS_URL, token=REDIS_TOKEN)
