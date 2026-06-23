39e8e5ca707eee3db1c41f9c08b4bdc638915483
Anisa Nabi <anisakhan5554@gmail.com>
2026-06-23 00:57:34 -0700

    Cleaned up redis client

--- core/redis_client.py
+++ core/redis_client.py
@@ -1,10 +1,12 @@
 import os
+from dotenv import  load_dotenv
 from upstash_redis import Redis
+
+load_dotenv()
 
 REDIS_URL = os.environ.get("REDIS_URL")
 REDIS_TOKEN = os.environ.get("REDIS_TOKEN")
 
-print("RAILWAY REDIS_URL:", repr(REDIS_URL))
-print("RAILWAY REDIS_TOKEN:", repr(REDIS_TOKEN))
+
 
 redis_client = Redis(url=REDIS_URL, token=REDIS_TOKEN)
